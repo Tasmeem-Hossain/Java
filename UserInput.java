b5294b281804a8fc48d76c6071be4c59c23897dc
Tasmeem Hossain <73355609+Tasmeem-Hossain@users.noreply.github.com>
2023-02-06 18:54:47 +0000

    Add files via upload

---
+++ UserInput.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author User
+ */
+import java.util.Scanner;
+public class UserInput {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int number;
+        number = input.nextInt();
+        System.out.print("Enter any Number: ");
+        System.out.println("Number: " +number);
+    }
+        
+}
