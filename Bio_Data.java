424ae5b52d2701670d825eca5114ba86579d0ad8
Tasmeem Hossain <73355609+Tasmeem-Hossain@users.noreply.github.com>
2023-02-21 13:29:08 +0000

    Add files via upload

---
+++ Bio_Data.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.basic_java;
+
+
+public class Bio_Data {
+    
+    public static void main(String[] args){
+        
+        System.out.println("Tanim Hassan");
+        System.out.print("07448964285");
+    }
+ }
+    
+
